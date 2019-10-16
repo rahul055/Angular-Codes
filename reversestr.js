24dad3ac52c56369c812984ca38ad37e8a7c61fd
rahul055 <rrjoshi055@gmail.com>
2019-10-16 11:58:11 +0530

    uploading for daily basis

---
+++ reversestr.js
@@ -0,0 +1,8 @@
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("rrjoshi055@gmail.com"));
+console.log(string_reverse("Google.com"));
+console.log(string_reverse("YouTube"));
