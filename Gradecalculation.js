e1d9012031b27fec45e0c035de7634a4c7bfe3f5
rahul055 <rrjoshi055@gmail.com>
2019-10-16 12:12:17 +0530

    Java homework

---
+++ Gradecalculation.js
@@ -0,0 +1,22 @@
+var avg = 98;
+
+if(avg<60)
+{
+    console.log("Grade :E");
+}
+else if(avg < 70)
+{
+    console.log("Grade :D");
+}
+else if(avg < 80)
+{
+    console.log("Grade :C");
+}
+else if(avg < 90)
+{
+    console.log("Grade :B");
+}
+else if(avg < 100)
+{
+    console.log("Grade :A");
+}
